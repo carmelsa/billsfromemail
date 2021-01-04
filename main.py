c38f25a6484cec48cd711e90cd93d1a8d1f3e752
carmelsa <carmelsahar1994@gmail.com>
2021-01-04 22:05:30 +0200

    gmail api

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-def hello():
-    print("Hello world")
+import pdf_handler
+import gmail_handler
 
 
 def main():
-    hello()
-
+    creds = gmail_handler.get_token()
+    emails = gmail_handler.get_attachment_from_email(creds)
+    all_payments=[]
+    for email in emails:
+        for attachment in email.pdf_attachments:
+            payment_amount = pdf_handler.get_payment_amount(attachment)
+            all_payments.append(payment_amount)
+    print(all_payments)
 
 if __name__ == '__main__':
-    main()
+     main()
